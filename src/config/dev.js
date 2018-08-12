e69bbc510b48a80e17051b8c378eaea85d8d208f
Ethan Nguyen <ethan.nguyens@gmail.com>
2018-08-12 13:56:04 +0100

    rm dev

--- src/config/dev.js
+++
@@ -1,8 +0,0 @@
-export const FirebaseConfig = {
-  apiKey: "AIzaSyCdBDIDVubEOm5ds2cjKuNlV6AEHrRjQJ0",
-  authDomain: "todo-react-firebase-en.firebaseapp.com",
-  databaseURL: "https://todo-react-firebase-en.firebaseio.com",
-  projectId: "todo-react-firebase-en",
-  storageBucket: "",
-  messagingSenderId: "1091903171088"
-};
