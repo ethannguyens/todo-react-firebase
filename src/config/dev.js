f9c7b2841afcc0188dba54125476f96738937592
ethan.nguyen <ethan.nguyens@gmail.com>
2018-08-12 13:31:03 +0100

    add firebase

---
+++ src/config/dev.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyCdBDIDVubEOm5ds2cjKuNlV6AEHrRjQJ0",
+  authDomain: "todo-react-firebase-en.firebaseapp.com",
+  databaseURL: "https://todo-react-firebase-en.firebaseio.com",
+  projectId: "todo-react-firebase-en",
+  storageBucket: "",
+  messagingSenderId: "1091903171088"
+};
